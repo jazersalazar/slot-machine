e0f1815a3bd83011963060af7403872d0351fcb5
Jazer Salazar <48846249+jazersalazar@users.noreply.github.com>
2022-04-02 09:47:58 +0800

    feat(core): add slot machine game logic

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -2,6 +2,13 @@ import Phaser from "phaser"
 
 export default class GameScene extends Phaser.Scene
 {
+    slots = [
+        ['cherry', 'blackberry', 'banana', 'banana', 'cherry', 'blackberry'],
+        ['banana', 'cherry', 'blackberry', 'cherry', 'banana', 'blackberry'],
+        ['blackberry', 'banana', 'cherry', 'banana', 'blackberry', 'cherry']
+    ]
+    reels = []
+
     constructor()
     {
         super('GameScene')
@@ -36,48 +43,97 @@ export default class GameScene extends Phaser.Scene
         this.background.displayWidth = this.sys.canvas.width
         this.background.displayHeight = this.sys.canvas.height
 
-        // setup symbol container
-        let slot = this.add.container(centerX, centerY)
+        // setup the reels
+        let reelPosX = [-191, 0, 200]
+        for (let x = 0; x < this.slots.length; x++) {
+            this.reels[x] = this.add.container(centerX + reelPosX[x], centerY)
+            this.reels[x].symbols = this.slots[x]
+            this.reels[x].position = 0
 
-        // add symbols
-        let banana = this.add.sprite(-190, 0, 'banana').setScale(0.5)
-        let blackberry = this.add.sprite(0, 0, 'blackberry').setScale(0.5)
-        let cherry = this.add.sprite(200, 0, 'cherry').setScale(0.5)
-        
-        slot.add([ cherry, banana, blackberry ])
+            for (let y = 0; y < this.slots[x].length; y++) {
+                let symbol = this.add.sprite(0, 0, this.slots[x][y]).setScale(0.5)
+                this.reels[x].add(symbol)
+            }
+        }
 
         // setup spin button
-        this.spin = this.add.sprite(centerX, centerY + 200, 'spin')
+        this.spinBtn = this.add.sprite(centerX, centerY + 200, 'spin')
             .setScale(0.5)
             .setInteractive()
             .on('pointerdown', () => this.startSpin())
         
         // setup bigwin text
-        this.bigwin = this.add.sprite(centerX, centerY - 170, 'bigwin')
+        this.bigwinTxt = this.add.sprite(centerX, centerY - 170, 'bigwin')
             .setScale(0.5)
-        this.bigwin.visible = false
+        this.bigwinTxt.visible = false
     }
 
     startSpin()
     {
-        this.bigwin.visible = false
+        this.bigwinTxt.visible = false
 
         // disable spin button until the spin process ends
-        this.spin.disableInteractive()
-        this.spin.setTint(0x808080)
+        this.spinBtn.disableInteractive()
+        this.spinBtn.setTint(0x808080)
 
-        setTimeout(() => { 
-            this.endSpin()
-        }, 1000)
+        // intialize stop time for the reels
+        let duration = Phaser.Math.FloatBetween(0, 3)
+
+        for (let x = 0; x < this.reels.length; x++) {
+            if (x > 0) {
+                let addTime = Phaser.Math.FloatBetween(1, 3)
+                this.reels[x].duration = this.reels[x - 1].duration + addTime
+            } else {
+                this.reels[x].duration = duration
+            }
+
+            this.reels[x].spin = this.time.addEvent({
+                delay: 100,
+                callback: () => { this.spin(x) },
+                callbackScope: this,
+                loop: true
+            })   
+        }
     }
 
-    endSpin()
+    spin(index) 
     {
-        //TODO: only show bigwin if there are 3 matching symbols
-        this.bigwin.visible = true
-        
+        let reel = this.reels[index]
+
+        // iterate through the symbol index
+        reel.position++
+        if (reel.position >= reel.symbols.length) {
+            this.reels[index].position = 0
+        }
+
+        // stop the event once the duration down to 0
+        reel.duration -= reel.spin.delay / 1000
+        if (reel.duration <= 0) {
+            reel.spin.remove()
+            
+            // call the endSpin if the last reels has been stopped
+            if (index == this.reels.length - 1) {
+                this.endSpin()
+            }
+        }
+
+        // apply the value changes to the actual objectj
+        this.reels[index] = reel
+    }
+
+    endSpin() {
+        let symbols = []
+        for (let x = 0; x < this.reels.length; x++) {
+            symbols[x] = this.reels[x].symbols[this.reels[x].position]
+        }
+
+        // display bigwin text
+        if (symbols[0] === symbols[1] && symbols[1] === symbols[2]) {
+            this.bigwinTxt.visible = true
+        }
+
         // reset spin button
-        this.spin.clearTint()
+        this.spinBtn.clearTint()
             .setInteractive()
     }
 }
